72ea75e3c9f2cdb6fa61361cc7c9abd831f99d1c
hagneva1 <hagneva1@di-722-16.iut.univ-lehavre.fr>
2019-11-12 10:12:30 +0100

    add entity vehicule and form for add vehicule in database

---
+++ src/Controller/VehiculeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehiculeController extends AbstractController
+{
+    /**
+     * @Route("/vehicule", name="vehicule")
+     */
+    public function index()
+    {
+        return $this->render('vehicule/index.html.twig', [
+            'controller_name' => 'VehiculeController',
+        ]);
+    }
+}
