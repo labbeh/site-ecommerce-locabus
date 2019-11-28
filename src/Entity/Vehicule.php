e887a0988d310ca52712f573f977c2b5513a095a
hagneva1 <hagneva1@dgs-13-15ab-1752.univ-rouen.fr>
2019-11-28 21:08:34 +0100

    add desc in vehicule table

--- src/Entity/Vehicule.php
+++ src/Entity/Vehicule.php
@@ -52,6 +52,11 @@ class Vehicule
      */
     private $critair;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -137,6 +142,18 @@ class Vehicule
     public function setCritair(int $critair): self
     {
         $this->critair = $critair;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
