0ffd6031b77b5720cb601a9cac07ecf9f667eb7b
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2019-12-14 20:55:28 +0100

    ajout d'une interface pour les params utilisateurs front uniquement

--- src/Controller/ParamUtilisateurController.php
+++ src/Controller/ParamUtilisateurController.php
@@ -4,7 +4,18 @@
 namespace App\Controller;
 
 
-class ParamUtilisateurController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+class ParamUtilisateurController extends AbstractController
+{
+    /**
+     * @Route("/utilisateur", name="utilisateur")
+     */
+    public function index()
+    {
+        return $this->render('utilisateur.html.twig', [
+            'controller_name' => 'ParamUtilisateur'
+        ]);
+    }
 }
