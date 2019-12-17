46d16951c09779ca4e7893c702c151bc563bcc02
hugo labbé <hugo.labbe@etu.univ-lehavre.fr>
2019-12-17 10:49:26 +0100

    ajout d'un commentaire

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -105,7 +105,7 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+        // pour garantir au moins le ROLE_USER à chaque utilisateur
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
