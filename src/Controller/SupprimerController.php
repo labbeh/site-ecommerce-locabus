c2aff26ea3d992afb74445249c055ba5be72eabf
hugo labbé <hugo.labbe@etu.univ-lehavre.fr>
2019-12-17 12:02:58 +0100

    controleur de suppresion

--- src/Controller/SupprimerController.php
+++ src/Controller/SupprimerController.php
@@ -4,7 +4,18 @@
 namespace App\Controller;
 
 
-class SupprimerController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+class SupprimerController extends AbstractController
+{
+    /**
+     * @Route("/supprimer", name="supprimer")
+     */
+    public function index($table, $id)
+    {
+        return $this->render('erreurdroits.html.twig', [
+            'controller_name' => 'SupprimerController',
+        ]);
+    }
 }
