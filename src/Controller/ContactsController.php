c45e99f62b323e87cb6b88b4ae750a87a2d4377b
hugo labbé <hugo.labbe@etu.univ-lehavre.fr>
2019-12-20 10:13:55 +0100

    ajout d'une page mentions légales

---
+++ src/Controller/ContactsController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactsController extends AbstractController
+{
+    /**
+     * @Route("/contacts", name="contacts")
+     */
+    public function index()
+    {
+        return $this->render('contacts.html.twig', ['controller_name' => 'ContactsController']);
+    }
+}
