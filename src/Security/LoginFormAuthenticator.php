ecd48e82183a4687b5b4222b339ce126b1016d5d
hagneva1 <hagneva1@dgs-13-15ab-1752.univ-rouen.fr>
2019-12-20 18:15:41 +0100

    update redirect after login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -86,7 +86,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($request->headers->get('referer'));
+        return new RedirectResponse($request->request->get('referer'));
     }
 
     protected function getLoginUrl()
